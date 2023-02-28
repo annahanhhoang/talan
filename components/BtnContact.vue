3643908387f35cba5e866804dca1c9b168527f9e
Anna Hoang <anna.hoang@rewardstyle.com>
2023-02-28 00:35:04 -0600

    :sparkle:feat: Home Page

--- components/BtnContact.vue
+++ components/BtnContact.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-btn class="text-capitalize text-decoration-none" color="primary" :to="to" variant="flat">
+  <v-btn
+    :class="`text-capitalize text-decoration-none ${textClass}`"
+    :color="color"
+    :to="to"
+    variant="flat"
+  >
     Contact Us
   </v-btn>
 </template>
@@ -10,6 +15,21 @@ export default {
     to: {
       type: String,
       default: '/contact',
+    },
+    color: {
+      type: String,
+      default: 'primary',
+    },
+
+    textColor: {
+      type: String,
+      default: undefined,
+    },
+  },
+
+  computed: {
+    textClass() {
+      return this.textColor ? `text-${this.textColor}` : '';
     },
   },
 };
