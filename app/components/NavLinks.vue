0562547db9b5e85e1e1c9efe7782af851767e73e
Anna Hoang <anna.hoang1322@gmail.com>
2025-04-07 15:44:06 -0500

    :fire:refactor: Remove solution page

--- app/components/NavLinks.vue
+++ app/components/NavLinks.vue
@@ -46,7 +46,6 @@ const links = [
   { title: 'Home', to: '/', type: 'link' },
   { title: 'Candidates', to: '/candidates', type: 'link' },
   { title: 'Client', to: '/client', type: 'link' },
-  { title: 'Clinical Research Solutions', to: '/solutions', type: 'link' },
   { title: 'About Us', to: '/about', type: 'link' },
   { title: 'Contact Us', to: '/contact', type: 'button' },
 ];
--- app/pages/solutions.vue
+++
@@ -1,1 +0,0 @@
-<template>Soluntion Page</template>
